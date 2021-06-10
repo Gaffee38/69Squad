9f4bda806ede92bbbcc58352e4f5f0207775d7d3
Gaffee38 <83159243+Gaffee38@users.noreply.github.com>
2021-06-10 15:16:29 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+$(function(){
+        $('.fuck').on('click', function(){
+            $('.fucked').toggleClass('active');
+        });
+});
+$(function(){
+        $('.poza').on('click', function(){
+            $('.buryatick').toggleClass('atacka');
+        });
+});
+$(function(){
+        $('.poza').on('click', function(){
+            alert('пошел нахуй')
+        });
+});
+$(function(){
+        $('.click').on('click', function(){
+            $('.cock1').toggleClass('sosok');
+        });
+});
+$(function(){
+        $('.burger').on('click', function(){
+            $('.cock2').toggleClass('sosok');
+        });
+});
+$(function(){
+        $('.pop').on('click', function(){
+            $('.cock3').toggleClass('sosok');
+        });
+});
+$(function(){
+        $('.cook').on('click', function(){
+            $('.cock4').toggleClass('sosok');
+        });
+});
+$(function(){
+        $('.mon').on('click', function(){
+            $('.cock5').toggleClass('sosok');
+        });
+});
